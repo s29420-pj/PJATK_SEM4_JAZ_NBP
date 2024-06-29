4a0a1636c7e6ea34bf6794f45519669380dfa5a7
Marek Lewanczyk <s29420@pjwstk.edu.pl>
2024-06-29 18:17:55 +0200

    Main App Class

---
+++ src/main/java/pl/pjatk/marlew/jazs29420nbp/JazS29420NbpApplication.java
@@ -0,0 +1,13 @@
+package pl.pjatk.marlew.jazs29420nbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JazS29420NbpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JazS29420NbpApplication.class, args);
+	}
+
+}
