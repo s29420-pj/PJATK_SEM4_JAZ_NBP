66001417ba1dc9acdc2e92af23c4f0ec2afa4e25
Marek Lewanczyk <s29420@pjwstk.edu.pl>
2024-06-29 18:23:02 +0200

    Updated AverageExchangeController

--- src/main/java/pl/pjatk/marlew/jazs29420nbp/controller/AverageExchangeController.java
+++ src/main/java/pl/pjatk/marlew/jazs29420nbp/controller/AverageExchangeController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.HttpServerErrorException;
+import pl.pjatk.marlew.jazs29420nbp.model.AverageExchange;
 import pl.pjatk.marlew.jazs29420nbp.repository.AverageExchangeRepository;
 import pl.pjatk.marlew.jazs29420nbp.service.AverageExchangeService;
 
@@ -20,11 +21,7 @@ public class AverageExchangeController {
     }
 
     @GetMapping("/avgBetweenDates/{valueName}/{startDate}/{endDate}")
-    public ResponseEntity<?> getAverageExchange(@PathVariable String valueName, @PathVariable String startDate, @PathVariable String endDate) {
-        try {
-            return ResponseEntity.ok(averageExchangeService.saveToDatabase(valueName, startDate, endDate));
-        } catch (HttpServerErrorException | HttpClientErrorException error) {
-            return new ResponseEntity<>(error.getStatusText(), error.getStatusCode());
-        }
+    public ResponseEntity<AverageExchange> getAverageExchange(@PathVariable String valueName, @PathVariable String startDate, @PathVariable String endDate) {
+        return ResponseEntity.ok(averageExchangeService.saveToDatabase(valueName, startDate, endDate));
     }
 }
