1cd24b14315e84d7a8fd26057623c6df5ef2598f
Marek Lewanczyk <s29420@pjwstk.edu.pl>
2024-06-29 18:44:20 +0200

    Added descriptions and example values for model.

--- src/main/java/pl/pjatk/marlew/jazs29420nbp/model/AverageExchange.java
+++ src/main/java/pl/pjatk/marlew/jazs29420nbp/model/AverageExchange.java
@@ -1,17 +1,30 @@
 package pl.pjatk.marlew.jazs29420nbp.model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
 
 @Entity
 @Table(name = "nbp_repository")
+@Schema(description = "AverageExchange reprezentuje model obiektu zapisywanego do bazy danych po każdym wykonanym zapytaniu")
 public class AverageExchange {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Schema(description = "Unikalny ID wykonanego zapytania", example = "1")
     private Integer id;
+
+    @Schema(description = "Kod waluty", example = "USD")
     private String valueName;
+
+    @Schema(description = "Początkowa data wykonywania zapytania od kiedy ma pobrać dane nbp.api.pl (YYYY-MM-DD)", example = "2024-06-10")
     private String startDate;
+
+    @Schema(description = "Końcowa data wykonywania zapytania do kiedy ma pobrać dane nbp.api.pl (YYYY-MM-DD) - DATA NIE MOŻE BYC WCZESNIEJSZA NIZ startDate", example = "2024-06-15")
     private String endDate;
+
+    @Schema(description = "Średnia wartość waluty w zadanym okresie czasu", example = "5.12572")
     private double average;
+
+    @Schema(description = "Data i godzina wykonanego zapytania", example = "2024-06-29T18:17:14.896992")
     private String queryDate;
 
     public AverageExchange() {}
